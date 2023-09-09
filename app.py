5cb9bdc94f237e71dc8b5699fefb71dc894d333b
Maya-Ghrayeb <131244615+Maya-Ghrayeb@users.noreply.github.com>
2023-09-09 20:28:02 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -11,10 +11,9 @@ import matplotlib.patches as mpatches
 
 api = SchonbergLabAPI(worker_key)
 
-
 app = Flask(__name__)
 
-def get_base64_graph():
+def get_base64():
     # Convert Matplotlib graph to base64 for rendering in HTML
     img = io.BytesIO()
     plt.savefig(img, format='png')
@@ -51,18 +50,32 @@ def get_last_session_data():
             # Check if data was successfully retrieved
             if session_data:
                 # Add the lines to request stress data here
-                early = datetime.now(timezone.utc) - timedelta(hours=24, minutes=0)
-                some_data = request_data("stressDetails", access_token=session_data['acc_token'],
+                early1 = datetime.now(timezone.utc) - timedelta(hours=24, minutes=0)
+                stress = request_data("stressDetails", access_token=session_data['acc_token'],
                                          access_token_secret=session_data['acc_token_secret'],
-                                         upload_start=early, upload_end=datetime.today(), is_backfill=False)
-                stress_data = list(some_data[-1]['timeOffsetStressLevelValues'].values())
-                bodyBattery_data = list(some_data[-1]['timeOffsetBodyBatteryValues'].values())
-                data.append(stress_data)
-                data.append(bodyBattery_data)
+                                         upload_start=early1, upload_end=datetime.today(), is_backfill=False)
 
-                # Print the stress data for verification
-                print("Stress Data:")
-                print(stress_data)
+                early2 = datetime.now(timezone.utc) - timedelta(hours=24, minutes=0)
+                dailies = request_data("dailies", access_token=session_data['acc_token'],
+                                         access_token_secret=session_data['acc_token_secret'],
+                                         upload_start=early2, upload_end=datetime.today(), is_backfill=False)
+
+                stress_data=[]
+                stress_data.append(list(stress[-1]['timeOffsetStressLevelValues'].keys()))
+                stress_data.append(list(stress[-1]['timeOffsetStressLevelValues'].values() ))
+
+                battery_data=[]
+                battery_data.append(list(stress[-1]['timeOffsetBodyBatteryValues'].keys()))
+                battery_data.append(list(stress[-1]['timeOffsetBodyBatteryValues'].values() ))
+
+                heart_data=[]
+                heart_data.append(list(dailies[-1]['timeOffsetHeartRateSamples'].keys()))
+                heart_data.append(list(dailies[-1]['timeOffsetHeartRateSamples'].values() ))
+
+                data.append(stress_data)
+                data.append(battery_data)
+                data.append(heart_data)
+
                 return data
             else:
                 # Handle the case where data retrieval fails
@@ -73,6 +86,88 @@ def get_last_session_data():
     else:
         # Handle the case where there are no sessions
         return None
+
+def get_base64_graph(graph_type):
+    # Generate the specified graph
+    if graph_type == "stress":
+        generate_stress_graph()
+    elif graph_type == "battery":
+        generate_battery_graph()
+    elif graph_type == "heart":
+        generate_heart_rate_graph()
+
+    # Save the graph as an image
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plt.clf()
+    return base64.b64encode(img.read()).decode()
+
+
+def generate_stress_graph():
+    data_stress = get_last_session_data()[0]
+    axisX_stress = []
+    for x in data_stress[0]:
+        axisX_stress.append(int(x) / 60)
+
+    axisY_stress = data_stress[1]
+
+    # Create a line graph for stress data
+    plt.figure(figsize=(6, 4))
+    plt.plot(axisX_stress, axisY_stress, marker='o', linestyle='-')
+    plt.xlabel('Time in minutes')
+    plt.ylabel('Stress')
+    plt.title('Stress Graph')
+
+    # Save the graph as an image
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    return base64.b64encode(img.read()).decode()
+
+def generate_battery_graph():
+    data_battery = get_last_session_data()[1]
+    axisX_battery = []
+    for x in data_battery[0]:
+        axisX_battery.append(int(x) / 60)
+
+    axisY_battery = data_battery[1]
+
+    # Create a line graph for bodyBattery data
+    plt.figure(figsize=(6, 4))
+    plt.plot(axisX_battery, axisY_battery, marker='o', linestyle='-')
+    plt.xlabel('Time in minutes')
+    plt.ylabel('Body Battery')
+    plt.title('Body Battery Graph')
+
+    # Save the graph as an image
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    return base64.b64encode(img.read()).decode()
+
+
+def generate_heart_rate_graph():
+    data_heart = get_last_session_data()[2]  # Assuming heart rate data is at index 2
+    axisX_heart = []
+    for x in data_heart[0]:
+        axisX_heart.append(int(x) / 60)
+
+    axisY_heart = data_heart[1]
+
+    # Create a line graph for heart rate data
+    plt.figure(figsize=(6, 4))
+    plt.plot(axisX_heart, axisY_heart, marker='o', linestyle='-')
+    plt.xlabel('Time in minutes')
+    plt.ylabel('Heart Rate')
+    plt.title('Heart Rate Graph')
+
+    # Save the graph as an image
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    return base64.b64encode(img.read()).decode()
+
 
 def get_bart_data(participant_id) -> Optional[Dict[str, Any]]:
     # Get all sessions using the original method
@@ -88,42 +183,24 @@ def get_bart_data(participant_id) -> Optional[Dict[str, Any]]:
         return None
 
 
+
+
 @app.route('/')
 def dashboard():
-    last_session_data_stress = get_last_session_data()[0]
-    last_session_data_battery = get_last_session_data()[1]
 
+    # Generate the base64-encoded stress graph
+    combined_graph_stress = get_base64_graph("stress")
 
-    # Customize x-axis ticks to show specific time values for both graphs
-    x_ticks = [0, 10, 20, 30, 40, 50, 60, 70]  # These represent data points
-    x_labels = ["10:00", "10:10", "10:20", "10:30", "10:40", "10:50", "11:00", "11:10"]  # Corresponding time labels
+    # Generate the base64-encoded bodyBattery graph
+    combined_graph_battery = get_base64_graph("battery")
 
-    # Create a subplot with two graphs (stress and bodyBattery)
-    plt.figure(figsize=(12, 6))
-
-    # Subplot for Stress Data
-    plt.subplot(1, 2, 1)
-    plt.plot(last_session_data_stress)
-    plt.xlabel('Time')
-    plt.ylabel('Stress Level')
-    plt.title('Stress Data Graph')
-    plt.xticks(x_ticks, x_labels)  # Apply custom ticks
-
-    # Subplot for Body Battery Data
-    plt.subplot(1, 2, 2)
-    plt.plot(last_session_data_battery)
-    plt.xlabel('Time')
-    plt.ylabel('Body Battery Level')
-    plt.title('Body Battery Data Graph')
-    plt.xticks(x_ticks, x_labels)  # Apply custom ticks
-
-    # Save the combined graph
-    combined_graph = get_base64_graph()
-
-    # Your data
+    # Generate the base64-encoded heart rate graph
+    combined_graph_heart = get_base64_graph("heart")
 
     bart_data = get_bart_data('eas@ew')
     bart_graph= None
+
+
     if bart_data is not None:
         # Extracting relevant data
         trial_types = [trial['trialType'] for trial in bart_data['data']]
@@ -132,7 +209,7 @@ def dashboard():
         response_times = [trial['responseTimes'] for trial in bart_data['data']]
 
         # Plotting sum of gains over trials
-        plt.figure(figsize=(15, 5))
+        plt.figure(figsize=(12, 4))
         plt.subplot(1, 3, 1)
         plt.plot(sum_of_gains, '-o')
         plt.title("Sum of Gains over Trials")
@@ -157,20 +234,19 @@ def dashboard():
         plt.xlabel("Trial")
         plt.ylabel("Response Time (ms)")
 
-        bart_graph = get_base64_graph()
+        bart_graph = get_base64()
 
-    # Render the template with the combined graph
-    return render_template('dashboard.html', combined_graph=combined_graph, bart_graph=bart_graph)
+
+        # Render the template with all graphs and show_welcome_sentences value
+    return render_template('dashboard.html', combined_graph_stress=combined_graph_stress,
+                            combined_graph_battery=combined_graph_battery,
+                            combined_graph_heart=combined_graph_heart,
+                            bart_graph=bart_graph)
+
 
 if __name__ == '__main__':
     # Get the data from the last session
     last_session_data = get_last_session_data()
-
-    if last_session_data:
-        # You can use last_session_data as needed in your main script
-        print("Data from the last session in the main script:")
-        print(last_session_data)
-
 
     # Start the Flask app
     app.run(debug=True, port=5001)
